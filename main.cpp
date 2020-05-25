b52c74c8ff49352cd1f1ab60d662fbb1e36ed0ce
Antonio Alexandru Ganea <antonio.ganea@yahoo.com>
2020-05-25 20:35:14 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// Antonio-Alexandru Ganea // Grupa 135 // Tema LFA3
+
+#include <iostream>
+#include <stdio.h>
+#include <map>
+using namespace std;
+
+class Grammar{
+    std::map<char, std::vector<int>> m;
+};
+
+int main(){
+
+}
